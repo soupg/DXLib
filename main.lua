b9697618b14c701d806767545dbcd98447af5a20
Alleexxi <66967403+Alleexxi@users.noreply.github.com>
2022-06-08 16:14:35 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -458,10 +458,94 @@ function dxl.ShowConsole(useless_variable_used_to_hook_dx9_show_console_function
         end
     end
 
+    function print_table(node) -- https://stackoverflow.com/a/42062321/19113503
+        local cache, stack, output = {},{},{}
+        local depth = 1
+        local output_str = "{\n"
+    
+        while true do
+            local size = 0
+            for k,v in pairs(node) do
+                size = size + 1
+            end
+    
+            local cur_index = 1
+            for k,v in pairs(node) do
+                if (cache[node] == nil) or (cur_index >= cache[node]) then
+    
+                    if (string.find(output_str,"}",output_str:len())) then
+                        output_str = output_str .. ",\n"
+                    elseif not (string.find(output_str,"\n",output_str:len())) then
+                        output_str = output_str .. "\n"
+                    end
+    
+                    -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+                    table.insert(output,output_str)
+                    output_str = ""
+    
+                    local key
+                    if (type(k) == "number" or type(k) == "boolean") then
+                        key = "["..tostring(k).."]"
+                    else
+                        key = "['"..tostring(k).."']"
+                    end
+    
+                    if (type(v) == "number" or type(v) == "boolean") then
+                        output_str = output_str .. string.rep('\t',depth) .. key .. " = "..tostring(v)
+                    elseif (type(v) == "table") then
+                        output_str = output_str .. string.rep('\t',depth) .. key .. " = {\n"
+                        table.insert(stack,node)
+                        table.insert(stack,v)
+                        cache[node] = cur_index+1
+                        break
+                    else
+                        output_str = output_str .. string.rep('\t',depth) .. key .. " = '"..tostring(v).."'"
+                    end
+    
+                    if (cur_index == size) then
+                        output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                    else
+                        output_str = output_str .. ","
+                    end
+                else
+                    -- close the table
+                    if (cur_index == size) then
+                        output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+                    end
+                end
+    
+                cur_index = cur_index + 1
+            end
+    
+            if (size == 0) then
+                output_str = output_str .. "\n" .. string.rep('\t',depth-1) .. "}"
+            end
+    
+            if (#stack > 0) then
+                node = stack[#stack]
+                stack[#stack] = nil
+                depth = cache[node] == nil and depth + 1 or depth - 1
+            else
+                break
+            end
+        end
+    
+        -- This is necessary for working with HUGE tables otherwise we run out of memory using concat on huge strings
+        table.insert(output,output_str)
+        output_str = table.concat(output)
+    
+        return output_str
+    end
+
+    
     function dxl.print(...)
         local temp = "";
         for i,v in pairs({...}) do
-            temp = temp..tostring(v).." "
+            if type(v) == "table" then
+                temp = temp..print_table(v).." "
+            else
+                temp = temp..tostring(v).." "
+            end
         end
         
         local split_string = {};
@@ -503,9 +587,9 @@ end
 
 --// Supg's attempt at making print() actually print as well as output to dxLib console (wish me luck)
 
-function double_print(sussy_variable)
-    dxl.OldPrint(sussy_variable);
-    dxl.print(sussy_variable);
+function double_print(...)
+    dxl.OldPrint(...);
+    dxl.print(...);
 end
 _G.print = double_print
 _G.error = dxl.error
